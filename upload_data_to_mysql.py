8539d0e065d483d40131d499c6faf0ff91e08f1c
Lê Huỳnh Thúy Vy <ivyle.data@gmail.com>
2025-03-20 05:52:10 +0700

    Update upload_data_to_mysql.py

--- upload_data_to_mysql.py
+++ upload_data_to_mysql.py
@@ -13,11 +13,15 @@ engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/{database}")
 # Đọc 2 dataset từ file CSV
 df1 = pd.read_csv("D:/TÀI LIỆU HỌC KỲ 8/Machine learning/Source Code/PycharmProjects/FinalProject/Dataset/movies.csv")  # Thay bằng tên file thực tế
 df2 = pd.read_csv("D:/TÀI LIỆU HỌC KỲ 8/Machine learning/Source Code/PycharmProjects/FinalProject/Dataset/ratings.csv")
+df3 = pd.read_csv("D:/TÀI LIỆU HỌC KỲ 8/Machine learning/Source Code/PycharmProjects/FinalProject/Dataset/user_account.csv")
 
 # Đẩy dataset1 lên MySQL thành bảng 'table1'
 df1.to_sql(name="movies", con=engine, if_exists="replace", index=False, chunksize=10000)
 
 # Đẩy dataset2 lên MySQL thành bảng 'table2'
 df2.to_sql(name="ratings", con=engine, if_exists="replace", index=False, chunksize=10000)
+
+# Đẩy dataset3 lên MySQL thành bảng 'table3'
+df3.to_sql(name="users", con=engine, if_exists="replace", index=False, chunksize=10000)
 
 print("✅ Đã đẩy dữ liệu lên MySQL thành công!")
