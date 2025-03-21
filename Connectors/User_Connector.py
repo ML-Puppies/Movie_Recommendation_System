dfb7082d5c1386f90a7ecf3b7251e9b387beccf5
hoangyen0501 <151611109+hoangyen51@users.noreply.github.com>
2025-03-22 00:33:48 +0700

    Update User_Connector.py

--- Connectors/User_Connector.py
+++ Connectors/User_Connector.py
@@ -8,7 +8,7 @@ class UserConnector(Connector):
         self.current_user_id = None # Stores the userId of the currently logged-in user for recommendation
 
     def login(self, userId, password):
-    """Authenticates login credentials, returns a User object if valid, otherwise returns None"""
+        """Authenticates login credentials, returns a User object if valid, otherwise returns None"""
         cursor = self.conn.cursor()
         sql = "SELECT * FROM user WHERE userID = %s AND password = %s"
         val = (userId, password)
