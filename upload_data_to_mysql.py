5bdec87841b8bdd5b581ad44883706c2dc83dd67
hoangyen0501 <151611109+hoangyen51@users.noreply.github.com>
2025-03-22 08:52:57 +0700

    Update upload_data_to_mysql.py

--- upload_data_to_mysql.py
+++ upload_data_to_mysql.py
@@ -1,27 +1,27 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# Thông tin kết nối đến MySQL
+# MySQL connection details
 user = "root"
 password = "Giang0409$"
-host = "localhost"  # Hoặc IP của MySQL Server
+host = "localhost" 
 database = "movie_ratings"
 
-# Tạo kết nối đến MySQL
+# Create a connection to the MySQL database
 engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}/{database}")
 
-# Đọc 2 dataset từ file CSV
-df1 = pd.read_csv("D:/TÀI LIỆU HỌC KỲ 8/Machine learning/Source Code/PycharmProjects/FinalProject/Dataset/movies.csv")  # Thay bằng tên file thực tế
+# Read datasets from CSV files
+df1 = pd.read_csv("D:/TÀI LIỆU HỌC KỲ 8/Machine learning/Source Code/PycharmProjects/FinalProject/Dataset/movies.csv")  
 df2 = pd.read_csv("D:/TÀI LIỆU HỌC KỲ 8/Machine learning/Source Code/PycharmProjects/FinalProject/Dataset/ratings.csv")
 df3 = pd.read_csv("D:/TÀI LIỆU HỌC KỲ 8/Machine learning/Source Code/PycharmProjects/FinalProject/Dataset/user_account.csv")
 
-# Đẩy dataset1 lên MySQL thành bảng 'table1'
+# Upload dataset1 to MySQL as the 'movies' table
 df1.to_sql(name="movies", con=engine, if_exists="replace", index=False, chunksize=10000)
 
-# Đẩy dataset2 lên MySQL thành bảng 'table2'
+# Upload dataset2 to MySQL as the 'ratings' table
 df2.to_sql(name="ratings", con=engine, if_exists="replace", index=False, chunksize=10000)
 
-# Đẩy dataset3 lên MySQL thành bảng 'table3'
+# Upload dataset3 to MySQL as the 'users' table
 df3.to_sql(name="users", con=engine, if_exists="replace", index=False, chunksize=10000)
 
-print("✅ Đã đẩy dữ liệu lên MySQL thành công!")
+print("Data has been successfully uploaded to MySQL!")
